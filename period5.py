1d54b75489c1c5bf1aa89e021756d906730edebb
Allison Drake <junesol.drake@gmail.com>
2023-09-06 19:46:43 +0000

    new message

--- period5.py
+++ period5.py
@@ -1,7 +1,1 @@
-y = input("Hi. Please print your favorite letter.")
-if y == "a":
-    print("basic")
-elif y == "b":
-    print("ur wrong")
-else: 
-    print("correct answer")
+print("I forgot to study for my math test...")
