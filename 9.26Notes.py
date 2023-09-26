c034374022df2468dc2bed5d92da0dec0946cea4
Allison Drake <junesol.drake@gmail.com>
2023-09-26 20:13:06 +0000

    notes from Sept 26, about algorithms

---
+++ 9.26Notes.py
@@ -0,0 +1,8 @@
+#Binary Search: time = O(logn) basically meaning that as you add more values, the time increases 
+    #logarithmically (slower than linear)
+    #list must be sorted
+    #goes through the list cutting it in half asking "is value greater than this point or less than"
+    #only has to evaluate half of the set of data
+
+#Linear Search: time = O(n) basically meaning that as you add values, time increases linearly
+    #goes through each value asking "is this the one"
