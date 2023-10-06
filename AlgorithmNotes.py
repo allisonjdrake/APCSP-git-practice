489f45b1c25eb8a8e05ed8c92db8e677c57e75e6
Allison Drake <junesol.drake@gmail.com>
2023-10-06 04:38:12 +0000

    Final Notes

--- AlgorithmNotes.py
+++ AlgorithmNotes.py
@@ -1,8 +1,9 @@
 #Binary Search: time = O(logn) basically meaning that as you add more values, the time increases 
-    #logarithmically (slower than linear)
+#logarithmically (slower than linear), time increases less per increased n
     #list must be sorted
     #goes through the list cutting it in half asking "is value greater than this point or less than"
     #only has to evaluate half of the set of data
+    #time (number of steps) = log based 2 of number of values
 
 #Linear Search: time = O(n) basically meaning that as you add values, time increases linearly
     #goes through each value asking "is this the one"
@@ -10,12 +11,15 @@
 #binary search: first index to pull from a list is len(list)/2 - 1
     # can create a new list where the indexes are either less than the OG or more than
 
-#password generator = exponential algorithm (time exponentially increase as the number of digits increases)
-#superpolynomial = impossible (runs faster than n^k)
+#When is MORE time better?
+    #password generator = exponential algorithm (time exponentially increase as the number of digits increases)
+
+#REASONABLENESS
+    #superpolynomial = impossible (runs faster than n^k)
     #unreasonable runs in super polynomial time (exponential, factorial time, and anything faster
     # reasonable does not run in super polynomial time (log, linear, quadratic)
 
-#heuristic: a rule that helps guide th algorithm to find good choices (good enough)
+#HEURISTIC: a rule that helps guide th algorithm to find good choices (good enough)
 
 #heuristic in real life: 
     #decision: not going on the freeway at times between 5 and 7 pm
