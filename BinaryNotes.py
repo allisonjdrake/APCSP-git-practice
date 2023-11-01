1a8e5c73efa6943b94e41a78d785a283d306d6df
Allison Drake <junesol.drake@gmail.com>
2023-11-01 20:23:55 +0000

    Notes from Nov 1 2023

---
+++ BinaryNotes.py
@@ -0,0 +1,46 @@
+# Our numbers = base 10
+Computer numbers = base 2 
+    On = 1
+    Off = 0
+
+BINARY TO DECIMAL - 
+Steps: 
+    Binary                   
+    Binary conversion:
+2^3 / 2^2 / 2^1 / 2^0
+
+Just like our system where we have:
+10^3 / 10^2 / 10^1 / 10^0
+Multiply times the conversion
+Add together  
+    
+EXAMPLES:   
+1 / 1 / 1 / 1
+2^3/2^2/2^1/2^0
+8 / 4 / 2 / 1  = 
+Answer: 1/1/1/1 converts to 15
+
+0 / 1 / 0 / 1
+8 / 4 / 2 / 1 
+0 / 4 / 0 / 1 = 5
+Answer: 101 converts to 5
+
+DECIMAL TO BINARY - 
+
+Enter: 37
+
+2^5 / 2^4 / 2^3 / 2^2 / 2^1 / 2^0
+ 32 /  16 /  8  / 4  / 2   / 1
+
+Answer: 1 / 0 / 0 / 1 / 0 / 1
+
+32 fits into 37 --> 5 left
+4 fits into 5 --> 1 left
+1 fits into 1 --> 0 left
+
+37 converts to 100101
+
+128 / 64 / 32 / 16 / 8 / 4 / 2 / 1
+1 0 0 1 0 0 0 0 
+
+144 - 128 = 16
