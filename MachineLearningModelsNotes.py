af221b82dac7190f749c55d2d7dce594dc775a39
Allison Drake <junesol.drake@gmail.com>
2023-11-30 21:10:11 +0000

    notes from 11/30/23

---
+++ MachineLearningModelsNotes.py
@@ -0,0 +1,15 @@
+# Unsupervised:
+#     Clustered - good for text data
+#         ex: set of tweets, want to figure out something about those tweets
+#         ex: tiktok algorithm
+#      Term Frequency: frequency of a term in a document (indicates how often a word occurs)
+#      Inverse Document Frequency: mportant ofa term (highlights low and rare common terms)
+#      TF-IDF: combines both
+# Supervised:
+#     Decision Tree:(best decision for me) tracks one variable to another to the output
+#         good for categorical data
+#     Random Forest: Multiple decision trees
+#     Linear Regression: Correlating independent and dependent
+#         usually best for things that have a variety of values (not categorical)
+#     Supervised Clustering: hey, here's what is right (has an "answer key")
+#         usually good for text
